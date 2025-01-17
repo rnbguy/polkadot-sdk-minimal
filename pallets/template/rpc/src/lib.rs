b066139dd56c1de1b61d5e3d21f88d76578be359
Ranadeep Biswas <mail@rnbguy.at>
2025-01-17 18:48:19 +0100

    pallet rpc

---
+++ pallets/template/rpc/src/lib.rs
@@ -0,0 +1,52 @@
+use jsonrpsee::{core::RpcResult, proc_macros::rpc, types::error::ErrorObject};
+pub use pallet_minimal_template_runtime_api::PalletMinimalTemplateApi as PalletMinimalTemplateApiRuntimeApi;
+use polkadot_sdk::sp_api::ProvideRuntimeApi;
+use polkadot_sdk::sp_blockchain::HeaderBackend;
+use polkadot_sdk::sp_runtime::traits::Block as BlockT;
+use std::sync::Arc;
+
+#[rpc(client, server)]
+pub trait PalletMinimalTemplateApi<BlockHash> {
+    #[method(name = "template_getValue")]
+    fn get_value(&self, at: Option<BlockHash>) -> RpcResult<u32>;
+}
+
+/// A struct that implements the `PalletMinimalTemplateApi`.
+pub struct PalletMinimalTemplate<C, Block> {
+    // If you have more generics, no need to TemplatePallet<C, M, N, P, ...>
+    // just use a tuple like TemplatePallet<C, (M, N, P, ...)>
+    client: Arc<C>,
+    _marker: std::marker::PhantomData<Block>,
+}
+
+impl<C, Block> PalletMinimalTemplate<C, Block> {
+    /// Create new `TemplatePallet` instance with the given reference to the client.
+    pub fn new(client: Arc<C>) -> Self {
+        Self {
+            client,
+            _marker: Default::default(),
+        }
+    }
+}
+
+impl<C, Block> PalletMinimalTemplateApiServer<<Block as BlockT>::Hash>
+    for PalletMinimalTemplate<C, Block>
+where
+    Block: BlockT,
+    C: Send + Sync + 'static + ProvideRuntimeApi<Block> + HeaderBackend<Block>,
+    C::Api: PalletMinimalTemplateApiRuntimeApi<Block>,
+{
+    fn get_value(&self, at: Option<<Block as BlockT>::Hash>) -> RpcResult<u32> {
+        let api = self.client.runtime_api();
+        let at = at.unwrap_or_else(|| self.client.info().best_hash);
+
+        api.get_value(at).map_err(runtime_error_into_rpc_err)
+    }
+}
+
+const RUNTIME_ERROR: i32 = 1;
+
+/// Converts a runtime trap into an RPC error.
+fn runtime_error_into_rpc_err(err: impl std::fmt::Debug) -> jsonrpsee::types::ErrorObject<'static> {
+    ErrorObject::owned(RUNTIME_ERROR, "Runtime error", Some(format!("{:?}", err)))
+}
